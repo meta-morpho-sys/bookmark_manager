832b76d6ad48e089ceb0c858a36909fb58fa070a
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-07-26 17:56:26 +0100

    Added a route to storing links into a database

--- app/app.rb
+++ app/app.rb
@@ -2,9 +2,22 @@ require 'sinatra/base'
 require_relative 'models/link'
 
 class BookmarkManager < Sinatra::Base
+  #Here we view the links
   get '/links' do
     @links = Link.all
     erb :'links/index'
+  end
+
+  # Here we get the form where we insert our links
+  get '/links/new' do
+    erb :'links/new_links'
+  end
+
+  # Here we create and store our links into a database. The links are extracted from the params.
+  # Then we are redirected to links to view the new collection of links
+  post '/links' do
+    Link.create(url: params[:url], title: params[:title])
+    redirect '/links'
   end
 
   run! if app_file == $0
--- spec/creating_links_spec.rb
+++ spec/creating_links_spec.rb
@@ -7,6 +7,15 @@ I would like to add the site's address and title to my bookmark manager
 feature 'Creating links' do
   scenario "I can add site's address and title" do
     visit '/links/new'
-    fill_in 'url', with: 'http://www.zom'
+    fill_in 'url', with: 'http://www.zombo.com'
+    fill_in 'title', with: 'This is Zombocom'
+    click_button 'Create link'
+
+    #we expect to be redirected to the links page
+    expect(current_path).to eq '/links'
+
+    within 'ul#links' do
+      expect(page).to have_content('This is Zombocom')
+    end
   end
 end
