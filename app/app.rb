ad520070299aa06e6d444e795c8b8ced5eee8596
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-07-27 15:16:49 +0100

    Added tagging feature

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,7 @@
 ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
-require_relative 'models/link'
+require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
   #Here we view the links
@@ -18,7 +18,11 @@ class BookmarkManager < Sinatra::Base
   # Here we create and store our links into a database. The links are extracted from the params.
   # Then we are redirected to links to view the new collection of links
   post '/links' do
-    Link.create(url: params[:url], title: params[:title])
+    link = Link.new(url: params[:url],
+                    title: params[:title])
+    tag = Tag.first_or_create(name: params[:tags])
+    link.tags << tag
+    link.save
     redirect '/links'
   end
 
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,17 +1,10 @@
-require 'data_mapper'
-require 'dm-postgres-adapter'
-
 #this class corresponds to a table in the database
 
 class Link
   include DataMapper::Resource
+
+  has n, :tags, through: Resource
   property :id,     Serial
   property :title,  String
   property :url,    String
 end
-
-
-#Setting up a connection with a database
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-DataMapper.finalize
-DataMapper.auto_upgrade!
