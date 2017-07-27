ee09ee76db2edd863d44422e4bc8b4094615d386
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-07-27 13:25:31 +0100

    General tidying

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,7 +33,7 @@ require 'simplecov-console'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
-
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
 
